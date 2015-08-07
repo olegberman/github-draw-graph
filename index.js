4e1d11e1127a93c16d47b61bb8032000cc8df219
Oleg Berman <bermanoleg@gmail.com>
2015-08-07 12:39:45 -0400

    The app now can do the commits 😈

---
+++ index.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var exec = require('child_process').exec;
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static('./'));
+
+var makeRepo = function(name, callback) {
+  exec('mkdir ' + __dirname + '/' + name, function() {
+    return exec('git init', {cwd: __dirname + '/' + name}, callback);
+  });
+};
+
+var randomFile = function(name, callback) {
+  return exec('echo ' + Math.random() + ' > gh', {
+    cwd: __dirname + '/' + name
+  }, callback);
+};
+
+var addCommit = function(name, date, callback) {
+  return exec('git add -A && git commit -am ":)" --date="' + date + '"', {
+    cwd: __dirname + '/' + name
+  }, callback);
+};
+
+app.post('/', function(req, res) {
+  var name = req.body.name || 'github-draw-graph';
+  var commits = req.body.commits || '';
+  commits = commits.split('\n');
+  makeRepo(name, function() {
+    var graphLoop = function() {
+      if(commits.length === 0) {
+        return res.send('ok');
+      }
+      var cur = commits.shift().split(':');
+      var amount = cur[0];
+      var date = cur[1];
+      var dayLoop = function() {
+        if(amount <= 0) {
+          return graphLoop();
+        }
+        randomFile(name, function() {
+          amount = amount - 1;
+          return addCommit(name, date, dayLoop);
+        });
+      };
+      dayLoop();
+    };
+    graphLoop();
+  });
+
+});
+
+app.listen(5000);
--- main.js
+++ main.js
@@ -19,23 +19,27 @@ var app = {
 
   makeSquares: function() {
     var weeks = Math.ceil(365 / 7),
-        emptyCells = 7 - this.today.getDay(),
+        firstIteration = true,
         counter = 365;
     while (weeks--) {
       var weekCol = document.createElement('div');
       weekCol.className = 'week-col';
-      while(emptyCells !== 0) {
-        weekCol.appendChild(this.makeCell('cell-empty'));
-        emptyCells = emptyCells - 1;
-      }
       while(weekCol.childNodes.length < 7) {
-        var cell = this.makeCell('cell');
         var date = new Date();
         var past = this.today.getDate() - counter;
         date.setDate(past);
+        if(firstIteration) {
+          var emptyCells = date.getDay();
+          while(emptyCells !== 0) {
+            weekCol.appendChild(this.makeCell('cell-empty'));
+            emptyCells = emptyCells - 1;
+          }
+          firstIteration = false;
+        }
         if(date > this.today) {
           break;
         }
+        var cell = this.makeCell('cell');
         cell.setAttribute('data-date', date);
         cell.addEventListener('click', this.cellClicked.bind(this));
         weekCol.appendChild(cell);
@@ -56,6 +60,7 @@ var app = {
     if(depth === 4) {
       cell.setAttribute('data-depth', '');
       cell.style.backgroundColor = '#eeeeee';
+      return true;
     }
     depth = depth + 1;
     cell.setAttribute('data-depth', depth);
@@ -68,7 +73,24 @@ var app = {
   },
 
   save: function() {
-    
+    var cells = document.getElementsByClassName('cell');
+    var name = document.getElementById('repo-name').value;
+    cells = Array.prototype.slice.call(cells);
+    var commits = cells.reduce(function(prev, cur, i) {
+      var depth = cells[i].getAttribute('data-depth');
+      if(parseInt(depth, 10)) {
+        return prev + depth + ':' + cells[i].getAttribute('data-date') + '\n';
+      } else {
+        return prev;
+      }
+    }, '');
+    $.post('/', {'name': name, 'commits': commits}, function(data) {
+      if(data === 'ok') {
+        //document.write('Awesome, now add the remote and push the ' + name + ' folder to Github');
+      } else {
+        //document.write('Something is wrong! 😨');
+      }
+    });
   }
 
 };
